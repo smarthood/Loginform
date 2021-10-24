1060423b0b3c20650cc74d47bdf2bca3806798bc
smarthood <renolddickson18@gmail.com>
2021-10-24 13:26:58 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,78 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyBgjd1kHVuk7n12fmB5bet0y-KQr0ZBQr0",
+  authDomain: "login-6d32f.firebaseapp.com",
+  projectId: "login-6d32f",
+  storageBucket: "login-6d32f.appspot.com",
+  messagingSenderId: "191266434336",
+  appId: "1:191266434336:web:90044bbcb0a529d881fb45"
+};
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const db = app.firestore();
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await auth.signInWithPopup(googleProvider);
+    const user = res.user;
+    const query = await db
+      .collection("users")
+      .where("uid", "==", user.uid)
+      .get();
+    if (query.docs.length === 0) {
+      await db.collection("users").add({
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const signInWithEmailAndPassword = async (email, password) => {
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await auth.createUserWithEmailAndPassword(email, password);
+    const user = res.user;
+    await db.collection("users").add({
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordResetEmail = async (email) => {
+  try {
+    await auth.sendPasswordResetEmail(email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  auth.signOut();
+};
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  signInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordResetEmail,
+  logout,
+};
